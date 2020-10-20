407772e801757951917c217a331a8bfc0bc98627
E2-Waite <E2-Waite>
2020-10-20 20:26:38 +0100

    Player death
    Player can now be killed - plays death animation.

---
+++ Dark Fantasy RPG/Assets/Scripts/AttackCollider.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackCollider : MonoBehaviour
+{
+    public float hitDelay = 0.3f;
+    public List<GameObject> enemies = new List<GameObject>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && !enemies.Contains(collision.gameObject))
+        {
+            enemies.Add(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D (Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && enemies.Contains(collision.gameObject))
+        {
+            enemies.Remove(collision.gameObject);
+        }
+    }
+
+    public void Hit(float damage)
+    {
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            if (enemies[i] == null)
+            {
+                enemies.Remove(enemies[i]);
+            }
+            else
+            {
+                enemies[i].GetComponent<EnemyController>().Damage(damage, hitDelay);
+            }
+        }
+    }
+
+}
---
+++ Dark Fantasy RPG/Assets/Scripts/EnemyCollider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollider : MonoBehaviour
+{
+    GameObject player = null;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player");
+            player = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player = null;
+        }
+    }
+
+    public void Hit(float damage)
+    {
+        if (player != null)
+        {
+            player.GetComponent<PlayerController>().Damage(damage);
+        }
+    }
+}
